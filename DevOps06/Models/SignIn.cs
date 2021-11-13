c825c3e29828778fceca58d0970211a067f2de27
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-11-13 14:20:53 +0100

    add summary

--- DevOps06/Models/SignIn.cs
+++ DevOps06/Models/SignIn.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace DevOps06.Models
 {
+    /// <summary>
+    /// The model for sign into the account.
+    /// </summary>
     public class SignIn
     {
+        /// <summary>
+        /// The user account password.
+        /// </summary>
         [Required]
         public string Password { get; set; }
 
+        /// <summary>
+        /// The user account username.
+        /// </summary>
         [Required]
         public string Username { get; set; }
     }
